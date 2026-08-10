b81cfc42d7655a76fbde17f65905e75b54c8c861
asher413 <a41337sh@gmail.com>
2026-08-10 17:22:48 +0300

    Delete store directory

--- store/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import SiteAnnouncement
-
-def global_settings(request):
-    return {
-        'announcement': SiteAnnouncement.objects.filter(is_active=True).first(),
-        'is_admin': request.user.is_superuser
-    }
